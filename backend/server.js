def5b0c264a2d8aba2c1386081b471479bf44138
Ruchir Agarwal <ruchiragarwal05@gmail.com>
2021-03-01 07:58:05 -0800

    Added authorization code & configured main file 'server.js'

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,31 @@
 'use strict';
 
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const path = require('path');
 
 const app = express();
 
-app.use('/users', (req,res)=>{
-    
+app.use((req, res, next) => {
+    const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
+    const origin = req.headers.origin;
+    console.log(origin);
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    console.log(req.headers);
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
+    return next();
 });
 
-const PORT = 5000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server listening at port#${PORT}`);
 })
---
+++ backend/utils/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+exports.checkAuth = (req, res, next) => {
+    const authkn = req.cookies.authkn;
+    if (!authtkn) {
+        return res.status(400).json({ msg: 'No auth token found. Please login again.' });
+    }
+    try {
+        let decoded = jwt.verify(authtkn, process.env.JWT_SECRET);
+        req.user = decoded;
+        console.log("Data: " , decoded);
+    } catch (error) {
+        return res.status(400).json({ msg: 'Invalid auth token. Please login again.' });
+    }
+    next();
+};
