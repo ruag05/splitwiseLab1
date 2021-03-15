b88053217f7700ff98c485ba70b4bf5ad9b09650
Ruchir Agarwal <ruchiragarwal05@gmail.com>
2021-03-15 14:40:37 -0700

    Improved Dashboard UI

--- frontend/src/pages/Dashboard/Sidebar.js
+++ frontend/src/pages/Dashboard/Sidebar.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from "react";
 import "./Sidebar.css";
 import { Link, NavLink } from "react-router-dom";
 import axios from "axios";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUsers } from "@fortawesome/free-solid-svg-icons";
+import { faFlag } from "@fortawesome/free-solid-svg-icons";
 const LeftNav = () => {
   let friends = ["Ruchir", "Praveen", "John", "Maria"];
   let usernames = ["Naren", "Amit", "Rahul", "Serena"];
-  //   let groups = ["Flat#4209", "SE 273", "Cahill Park"];
 
   const [grps, setGrps] = useState([]);
   useEffect(() => {
@@ -21,37 +22,42 @@ const LeftNav = () => {
           });
         });
       })
-      .catch((err) => {});
+      .catch((err) => { });
   }, []);
   return (
     <nav className="left-nav">
-      <NavLink to="/dashboard" activeClassName="is-active" className="dashlink">
-        {" "}
+      <NavLink to="/dashboard" className="dashlink" activeClassName="is-active">        
         <div className="logoimg2"></div>Dashboard
       </NavLink>
       <NavLink to="/activity" className="dashlink" activeClassName="is-active">
         {" "}
-        <i className="fas fa-flag"></i>Recent Activity
+        <span className="mx-2">
+          <FontAwesomeIcon icon={faFlag} color="grey"/>
+        </span>
+        Recent Activity
       </NavLink>
-      <h1 className="friends">
+      <h1 className="headers">
         Groups
-        <button className="add-group-button">+ add</button>
+        <button className="add-button">+ add</button>
       </h1>
       {grps.map((g) => (
         <p key={g.id}>
-          <Link to={`/groups/${g.id}`}>{g.name}</Link>
+          <Link className="eachgroup" to={`/groups/${g.id}`}>
+            <span className="mx-2">
+              <FontAwesomeIcon icon={faUsers} color="grey"/>
+            </span>
+            {g.name}
+          </Link>
         </p>
       ))}
-      <h1 className="friends">
+      <h1 className="headers">
         Friends
-        <button className="add-friend-button">+ add</button>
+        <button className="add-button">+ add</button>
       </h1>
       {friends}
-      <h1 className="friends">Friend Requests</h1>
-
-      <h1 className="friends">All Users</h1>
+      <h1 className="headers">Invites Requests</h1>
+      <h1 className="headers">All Users</h1>
       {usernames}
-
       <nav className="invitenav">
         <h1 className="invitefriends">Invite Friends to Splicewise</h1>
         <input
